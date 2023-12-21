30b0532645a275ee6c9ff2a05250af82cf6f6335
Neringa Chomanskyte <chomanskyten@gmail.com>
2023-12-21 16:48:38 +0000

    rebase some minor changes

---
+++ create/creat_user_class.py
@@ -0,0 +1,17 @@
+class Player:
+    def __init__(self, username):
+        self.username = username
+        self.score = 0
+    
+    def set_score()
+
+    def add_points()
+
+    def save(self):
+        users = load_list_dict_from_csv("../data/users.csv")
+            for row in row["name".strip().lower() == username.strip().lower():
+                row["score"] = self.score
+                if
+
+if __name__ = "__main""":
+    logging.basicConfig(filename="my_log.log")
---
+++ create/create_user_file.py
@@ -0,0 +1,4 @@
+with open("./files/serpent_academy_leaderboard.csv", "w") as file_object:
+    file_object.write("name,password")
+
+print("Users csv file initiated")
