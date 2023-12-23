9a020019cf8e9b61cb99e68107dc600bf5636e3b
meshadmh <147321583+meshadmh@users.noreply.github.com>
2023-12-23 16:24:12 +0000

    Create teacher class wip.py

---
+++ teacher class wip.py
@@ -0,0 +1,9 @@
+class Teacher:
+    def __init__ (self,name,subject):
+
+        self.name = name
+        self.name = hp
+        self.subject = subject
+
+    def teach(self):
+        print(f"{self.name}")
